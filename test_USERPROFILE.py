1fde0f18e33b44fe91f6ce171670c4f5ddee85e9
Monkgogi <zcemmpg@ucl.ac.uk>
2021-01-12 18:44:35 +0000

    Added Monkgogi.py

--- test_USERPROFILE.py
+++ test_USERPROFILE.py
@@ -2,5 +2,11 @@ from unittest import TestCase
 
 
 class TestUSERPROFILE(TestCase):
+    def test__country(self):
+        self.fail()
+
     def test_set_description(self):
+        self.fail()
+
+    def test_edit_profile_picture(self):
         self.fail()
