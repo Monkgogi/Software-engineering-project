13568f8e65d2e3c57b598d2ab6a1b459e15cbca6
tiff413 <thisistiffanyyu@gmail.com>
2021-01-12 12:13:33 +0000

    flask skeleton and comments class

---
+++ classes/comments.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+class Comment:
+    def __init__(self, userID, text, score=None):
+        self.userID = userID
+        self.fulfilmentText = text
+        self.score = score
+        self.time = datetime.now()
+
+    def upvote(self):
+        """Upvote comment"""
+
+    def downvote(self):
+        """Downvote comment"""
+
+    def remove(self):
+        """Remove comment"""
+
+    def make_anon(self):
+        """Make comment anonymous"""
+
+    def __repr__(self):
+        return str(self.userID)+": "+str(self.fulfilmentText)
