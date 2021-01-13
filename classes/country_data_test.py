5963acbca94c133e1d4852c5653afde67ed6fc86
acoyen4 <acoyne4@gmail.com>
2021-01-12 20:34:33 -0500

    added assert statement

--- classes/country_data_test.py
+++ classes/country_data_test.py
@@ -1,21 +1,24 @@
+from CountryData import *
 import unittest
 
 
-class CountryData(unittest.TestCase):
-    def get_info_for_year(self, year):
+class TestCountryData(unittest.TestCase):
+    def test_get_info_for_year(self, year):
         """
-        List should have country,
+        List should have country, travel purpose
         total visits, total nights,
         total spend, and average stay duration
         """
+        c = CountryData("Test", 1, 1, 1, 1, 1)
+        self.assertEqual(len(c.get_travel_purpose_vals()), 6)
 
-    def info_for_year_and_quarter(self, year, quarter):
+    def test_info_for_year_and_quarter(self, year, quarter):
         """
         List should have three positive values:
         total visits, total nights, total spend
         """
 
-    def get_travel_purpose_vals(self):
+    def test_get_travel_purpose_vals(self):
         """
         List should have only five positive
         values for travel purposes
